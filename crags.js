8b0edb671c34e4cd6791e76ed89b0c591085c5eb
Valerio Galli <valerio@ephos.io>
2026-07-12 21:44:02 +0200

    Improved visits

--- crags.js
+++ crags.js
@@ -3,6 +3,10 @@
    Only name + lat + lng are required; the rest are optional flavour.
    ===================================================================== */
 const CRAGS = [
-    {"type": "crag","name": "Muro del Butch","lat": 45.908774,"lng": 9.434212,"rock": "Limestone","link": "https://larioclimb.paolo-sonja.net/falesie_lecco/ilmurodelbutch/index.html"},
+    {"type": "crag","name": "Muro del Butch","lat": 45.908774,"lng": 9.434212,"rock": "Limestone","link": "https://larioclimb.paolo-sonja.net/falesie_lecco/ilmurodelbutch/index.html",
+      visits: [
+        {"date":"2026-07-11","with":"Irene","routes":[{"grade":"6c","comment":"primo 6c!!! alla scala dei sogni l'unico che c'è"},{"grade":"6b","comment":"alla scala dei sogni il solito molto bello e fisico"},{"grade":"6b","comment":"al muretto del Gio di fianco al 6c, molto carino e continuo di placca a tacche, fine atletica in strapiombo"}]},
+      ]
+    },
     { type: "crag", name: "Falesia di Civate", lat: 45.828727, lng: 9.335894, parkingLat: 45.828727, parkingLng: 9.335894, rock: "Limestone", link: "https://larioclimb.paolo-sonja.net/falesie_lecco/civate/index.html" },
 ];

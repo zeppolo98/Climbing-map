22202cf5e21739c16368f3b920c01ba6f4d75252
Valerio Galli <valerio@ephos.io>
2026-07-16 19:14:10 +0200

    Added some visits

--- crags.js
+++ crags.js
@@ -3,10 +3,147 @@
    Only name + lat + lng are required; the rest are optional flavour.
    ===================================================================== */
 const CRAGS = [
-    {"type": "crag","name": "Muro del Butch","lat": 45.908774,"lng": 9.434212,"rock": "Limestone","link": "https://larioclimb.paolo-sonja.net/falesie_lecco/ilmurodelbutch/index.html",
-      visits: [
-        {"date":"2026-07-11","with":"Irene","routes":[{"grade":"6c","comment":"primo 6c!!! alla scala dei sogni l'unico che c'è"},{"grade":"6b","comment":"alla scala dei sogni il solito molto bello e fisico"},{"grade":"6b","comment":"al muretto del Gio di fianco al 6c, molto carino e continuo di placca a tacche, fine atletica in strapiombo"}]},
-      ]
-    },
-    { type: "crag", name: "Falesia di Civate", lat: 45.828727, lng: 9.335894, parkingLat: 45.828727, parkingLng: 9.335894, rock: "Limestone", link: "https://larioclimb.paolo-sonja.net/falesie_lecco/civate/index.html" },
+  {
+    "type": "crag",
+    "name": "Muro del Butch",
+    "lat": 45.908774,
+    "lng": 9.434212,
+    "rock": "Limestone",
+    "link": "https://larioclimb.paolo-sonja.net/falesie_lecco/ilmurodelbutch/index.html",
+    "visits": [
+      {
+        "date": "2026-07-11",
+        "with": "Irene",
+        "routes": [
+          {
+            "name": "Su alto",
+            "grade": "6c",
+            "comment": "primo 6c!!! alla scala dei sogni l'unico che c'è"
+          },
+          {
+            "name": "Busazza",
+            "grade": "6b",
+            "comment": "alla scala dei sogni il solito molto bello e fisico"
+          },
+          {
+            "name": "Al guanaco",
+            "grade": "6b",
+            "comment": "al muretto del Gio di fianco al 6c, molto carino e continuo di placca a tacche, fine atletica in strapiombo"
+          },
+          {
+            "name": "L'amata grigna",
+            "grade": "6a",
+            "comment": "alla scala dei sogni, tecnico molto bello"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "type": "crag",
+    "name": "Falesia di Civate",
+    "lat": 45.828727,
+    "lng": 9.335894,
+    "parkingLat": 45.828727,
+    "parkingLng": 9.335894,
+    "rock": "Limestone",
+    "link": "https://larioclimb.paolo-sonja.net/falesie_lecco/civate/index.html",
+    "visits": [
+      {
+        "date": "2026-06-12",
+        "with": "SaraG",
+        "routes": [
+          {
+            "name": "Donne in attesa",
+            "grade": "6a",
+            "comment": "Belo, lungo, un po' untino il passagio duro sotto"
+          },
+          {
+            "name": "Via Nomentana",
+            "grade": "6b",
+            "comment": "Bello, lungo, duro sotto. Allungo al limite"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "type": "crag",
+    "name": "Val di Mello (blocchi)",
+    "lat": 46.249768,
+    "lng": 9.64196,
+    "parkingLat": 46.23945,
+    "parkingLng": 9.627467,
+    "rock": "Granite",
+    "link": "https://www.thecrag.com/it/arrampicata/italy/val-di-mello/area/1279720257",
+    "visits": [
+      {
+        "date": "2026-07-05",
+        "with": "Ire, Gipi, SaraS, AndreC",
+        "routes": [
+          {
+            "name": "Masso dei veronesi",
+            "grade": "6c",
+            "comment": "Primo blocco 6c! Link del 6a+ sit e del classico 6a stand"
+          },
+          {
+            "name": "Muro dei Granchi",
+            "grade": "6a+",
+            "comment": "uscito a destra, duro"
+          },
+          {
+            "name": "Muro dei Granchi",
+            "grade": "6b",
+            "comment": "Allungo e via"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "type": "multipitch",
+    "name": "Placca di Pinocchio (Ponte Brolla)",
+    "lat": 46.189872,
+    "lng": 8.756805,
+    "parkingLat": 46.189872,
+    "parkingLng": 8.756805,
+    "rock": "Granite",
+    "link": "https://www.sassbaloss.com/pagine/uscite/harlem/harlem.htm",
+    "visits": [
+      {
+        "date": "2026-06-27",
+        "with": "Japo, SaraG, Ire, Luca",
+        "routes": [
+          {
+            "name": "Via Harlem",
+            "grade": "5b",
+            "comment": "Tutta placca, molto sporca (questo ha alzato la difficoltà parecchio perchè si scivolava). Un cado boia, da fare in autunno/primavera, è in ombra fino alle 11 circa"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "type": "crag",
+    "name": "Falesia Esigo-Ponte Romano",
+    "lat": 46.282635,
+    "lng": 8.287281,
+    "parkingLat": 46.285113,
+    "parkingLng": 8.287014,
+    "rock": "Granite",
+    "link": "https://www.gulliver.it/itinerari/esigo-ponte-romano-falesia/",
+    "visits": [
+      {
+        "date": "2026-06-28",
+        "with": "Japo, Ire, Luca",
+        "routes": [
+          {
+            "name": "Deverino Valley",
+            "grade": "5c",
+            "comment": "Carina, passaggio non banale sotto poi placca di equilibrio"
+          }
+        ]
+      }
+    ]
+  }
 ];
